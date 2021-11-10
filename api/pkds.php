eff5687396464bace0e26d3616909eebc6275041
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-11-10 12:11:30 -0300

    Update pkds.php

--- api/pkds.php
+++ api/pkds.php
@@ -1,5 +1,5 @@
 <?php
-    $abc =  json_decode(file_get_contents('https://ff-advance.ff.garena.com/api/index?lang=en'));
+    $abc =  json_decode(file_get_contents('https://ff-advance.ff.garena.com/api/index?lang=pt'));
     echo "Registro: " . date("d/m",$abc->register_time[0]). ' até '.date("d/m",$abc->register_time[1])."   ";
     echo "Teste: " . date("d/m",$abc->playtest_time[0]). ' até '.date("d/m",$abc->playtest_time[1]);
 ?>
