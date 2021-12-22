0b6aa224dd2d00ca26c7334e1afc6fe344979433
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-12-22 20:40:21 -0300

    Add files via upload

---
+++ api/eventos-to-img.php
@@ -0,0 +1,31 @@
+<form method = 'post'>
+<input type = 'text' name = 'capt'>
+<button>Capturar Imagens Do web Evento</button>
+</form>
+
+<?php
+
+$link = $_POST['capt'];
+$token = parse_url($link)['query'];
+$regiao_separa = explode('&lang=',$token);
+$regiao = $regiao_separa[1];
+$nome_evento = parse_url($link)['host'];
+
+$api = file_get_contents('https://'.$nome_evento.'/api/info?'.$token.$regiao);
+
+$linkapi = explode("https",$api);
+for($b = 0; $b<count($linkapi)-1;$b++){
+	$nada = explode('",',$linkapi[$b]);
+	echo "<img src = 'https".$nada[0]."'/>";
+	echo "<br>";
+}
+
+$api_css = file_get_contents('https://'.$nome_evento.'/api/css?'.$token.$regiao);
+$convert = explode(")",$api_css);
+for($a = 0; $a<count($convert)-1;$a++){
+	$outro = explode("(",$convert[$a]);
+	echo '<img src = '.$outro[1].'/>';
+	echo "<br>";
+}
+?>
+
