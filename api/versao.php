ff79152530897717b8ea737052814f17f1422341
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-12-29 21:16:45 -0300

    Update versao.php

--- api/versao.php
+++ api/versao.php
@@ -1,5 +1,5 @@
 <?php
 $linkplay = file_get_contents("https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR");
 $sep1 = explode('class="IQ1z0d"><span class="htlgb">',$linkplay);
-echo $sep1[1];
+echo $sep1[4];
 ?>
