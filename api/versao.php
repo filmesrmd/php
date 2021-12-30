27509a3c81b5628088a31bc2aba4ecf397740925
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-12-29 21:22:01 -0300

    Update versao.php

--- api/versao.php
+++ api/versao.php
@@ -3,5 +3,8 @@ $linkplay = file_get_contents("https://play.google.com/store/apps/details?id=com
 $sep1 = explode('class="IQ1z0d"><span class="htlgb">',$linkplay);
 $sep2 = explode('</span></div></span',$sep1[4]);
 
-echo $sep2[0];
+$versao = $sep2[0];
+
+$apiv = json_decode(file_get_contents('https://version.common.freefiremobile.com/live/ver.php?version=$versao&lang=pt-br&device=android&channel'));
+echo $apiv->remote_version;
 ?>
