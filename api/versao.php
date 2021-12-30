6901bd483eab6fcfd7d2f1ff337442ef0a0f6480
filmesrmd <59299505+filmesrmd@users.noreply.github.com>
2021-12-29 21:13:41 -0300

    Update versao.php

--- api/versao.php
+++ api/versao.php
@@ -1,3 +1,5 @@
 <?php
-echo file_get_contents("https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR");
+$linkplay = file_get_contents("https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR");
+$sep1 = explode('class="IQ1z0d"><span class="htlgb">',$linkplay);
+echo $sep1[1];
 ?>
